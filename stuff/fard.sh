ba5f5769c21987631986954efb735d0b2d11022a
fartcan6 <121769974+fartcan6@users.noreply.github.com>
2023-03-07 00:49:24 +0800

    Update fard.sh

--- stuff/fard.sh
+++ stuff/fard.sh
@@ -6,7 +6,7 @@ if [[ "$1" == "--install" ]]; then
 
 
   # change into the "stuff" directory
-  cd fard/stuff/fard.sh
+  cd fard/stuff
 
   if [ $(($RANDOM % 2)) -eq 0 ]; then
     echo "Installing package... 0%"
